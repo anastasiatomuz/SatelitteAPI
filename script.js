3893122348d410e82d05e3fa07f3c9109e2cb764
anastasiatomuz <101012519+anastasiatomuz@users.noreply.github.com>
2023-12-18 10:24:54 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -12,11 +12,31 @@ async function getLoc(){
     document.getElementById('lon').innerHTML = longitude;
     document.getElementById('vel').innerHTML = velocity;
 
-    //updates content every second 
-    //credit: https://stackoverflow.com/questions/32913226/auto-refresh-page-every-30-seconds
-    window.setTimeout( function() {
-        window.location.reload();
-      }, 1000);
+     // ADD THE MARKER HERE and insert our lat and lon from the API itself
+     marker.setLatLng([latitude, longitude]);
+     map.setView([latitude, longitude],2)
 }
 
 getLoc();
+
+let map = L.map('iss_loc').setView([0, 0], 1); // 0 lat and long, no zoom zoom =1;
+
+
+const marker = L.marker([0, 0]).addTo(map); // this makes the marker starting at 0,0
+
+
+//we need the attribution as stated on the leaflet page
+const attribution =
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+
+// not a valid URL, more of a format
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+
+//now we add the tiles to our map
+tiles.addTo(map);
+
+
+
+setInterval(getLoc, 1000);
